7e1f8c3d28f1abf2efec951f1629d5a091e27d6e
Steven Sheehy <steven.sheehy@hedera.com>
2020-03-09 12:41:16 -0500

    Update test to fail for 0.8.2 snapshot
    Signed-off-by: Steven Sheehy <steven.sheehy@hedera.com>

--- src/test/java/com/example/r2dbc/example/R2DbcExampleApplicationTests.java
+++ src/test/java/com/example/r2dbc/example/R2DbcExampleApplicationTests.java
@@ -2,6 +2,7 @@ package com.example.r2dbc.example;
 
 import junit.framework.TestCase;
 import lombok.Data;
+import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -17,10 +18,13 @@ import org.springframework.data.r2dbc.query.Criteria;
 import org.springframework.scheduling.annotation.Schedules;
 import org.springframework.test.context.ContextConfiguration;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.shaded.com.google.common.util.concurrent.Uninterruptibles;
 import org.w3c.dom.css.Counter;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
+import reactor.retry.Jitter;
+import reactor.retry.Repeat;
 import reactor.test.StepVerifier;
 
 import javax.annotation.PreDestroy;
@@ -29,8 +33,11 @@ import java.security.SecureRandom;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.Random;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -42,7 +49,7 @@ import java.util.stream.Stream;
 @Slf4j
 class R2DbcExampleApplicationTests {
 
-	private static final int MESSAGE_COUNT = 10_000;
+	private static final int MESSAGE_COUNT = 50_000;
 
 	@Resource
 	private DatabaseClient databaseClient;
@@ -63,14 +70,15 @@ class R2DbcExampleApplicationTests {
 		Collection<TestCase> testCases = new ArrayList<>();
 		Collection<TestCase> unverified = new ArrayList<>();
 
-		for (int i = 1; i < 15; i++) {
+		for (int i = 1; i < 4; i++) {
 			testCases.add(queryAndCancel(i));
 		}
 
 		for (TestCase testCase : testCases) {
 			try {
 				log.info("Verifying test case: {}", testCase);
-				testCase.getStepVerifier().verify(Duration.ofSeconds(3));
+				testCase.getStepVerifier().verify();
+				Assertions.assertIterableEquals(LongStream.range(1, testCase.getLimit() + 1).boxed().collect(Collectors.toList()), testCase.getValues());
 			} catch (Throwable e) {
 				log.warn("Error for {}: {}", testCase, e.getMessage());
 				unverified.add(testCase);
@@ -82,36 +90,46 @@ class R2DbcExampleApplicationTests {
 	}
 
 	private TestCase queryAndCancel(int id) {
-		Criteria whereClause = Criteria.where("realm_num")
+		Criteria.CriteriaStep whereClause = Criteria.where("realm_num")
 				.is(0)
 				.and("topic_num")
 				.is(0)
-				.and("consensus_timestamp")
-				.greaterThanOrEquals(0);
+				.and("consensus_timestamp");
 
-		int limit = ThreadLocalRandom.current().nextInt(MESSAGE_COUNT);
+		int limit = MESSAGE_COUNT;//ThreadLocalRandom.current().nextInt(MESSAGE_COUNT);
 		AtomicLong counter = new AtomicLong();
+		Collection<Long> values = new ConcurrentLinkedQueue<>();
 
-		StepVerifier stepVerifier = databaseClient.select()
-				.from(TopicMessage.class)
-				.matching(whereClause)
-				.orderBy(Sort.by("consensus_timestamp"))
-				.page(PageRequest.of(0, MESSAGE_COUNT))
-				.fetch()
-				.all()
+		StepVerifier stepVerifier = Flux.defer(() -> databaseClient.select()
+					.from(TopicMessage.class)
+					.matching(whereClause.greaterThan(counter.get()))
+					.orderBy(Sort.by("consensus_timestamp"))
+					.page(PageRequest.of(0, 1000))
+					.fetch()
+					.all()
+					.doOnSubscribe(s -> log.debug("[{}] Executing query with {}/{} messages", id, counter, limit)))
+				.repeatWhen(Repeat.times(Long.MAX_VALUE)
+						.fixedBackoff(Duration.ofSeconds(1))
+						.jitter(Jitter.random(0.2)))
 				.as(t -> t.limitRequest(limit))
 				.doOnSubscribe(s -> log.info("[{}] Executing query with limit {}", id, limit))
 				.doOnCancel(() -> log.info("[{}] Cancelled query with {}/{} messages", id, counter.get(), limit))
+				.doOnCancel(() -> {System.exit(1);})
 				.doOnComplete(() -> log.info("[{}] Completed query with {}/{} messages", id, counter.get(), limit))
 				.doOnNext(t -> log.trace("[{}] onNext: {}", id, t))
 				.doOnNext(t -> counter.incrementAndGet())
+				.doOnNext(t -> Uninterruptibles.sleepUninterruptibly(1, TimeUnit.MILLISECONDS)) // Simulate client backpressure
 				.map(TopicMessage::getConsensusTimestamp)
+				.doOnNext(values::add)
+				.timeout(Duration.ofSeconds(30))
+				.publishOn(Schedulers.boundedElastic())
+				.subscribeOn(Schedulers.elastic())
 				.as(StepVerifier::create)
-				.expectNextSequence(LongStream.range(1, limit + 1).boxed().collect(Collectors.toList()))
+				.expectNextCount(limit)
 				.expectComplete()
 				.verifyLater();
 
-		return new TestCase(stepVerifier, id, limit, counter);
+		return new TestCase(stepVerifier, id, limit, counter, values);
 	}
 
 	@Data
@@ -120,6 +138,8 @@ class R2DbcExampleApplicationTests {
 		private final int id;
 		private final int limit;
 		private final AtomicLong count;
+		@ToString.Exclude
+		private final Collection<Long> values;
 	}
 
 	@TestConfiguration
